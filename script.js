66a9b46bba8a481316ec37ce2b18348475540e09
thesmahmud77 <mahmudsazzad77@gmail.com>
2025-12-06 00:31:42 -0800

    2nd push

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+const categoryContainer = document.getElementById("category-Container");
+
 const loadCategory = () => {
   fetch("https://news-api-fs.vercel.app/api/categories") //promise
     .then((res) => res.json()) //response recived
     .then((data) => {
       //convert to the promise
-      console.log(data);
+      console.log(data.categories);
+      const categoris = data.categories;
+      const categories = data.categories;
+      categories.forEach(categories.tittle => {
+        
+      });
     })
     .catch((err) => {
       console.log(err);
